a5bc0a73cd6cbc966cd064e6741c0ebcb7458c10
osmangonirabbi13 <osmangoniyou12@gmail.com>
2026-03-26 12:16:09 +0600

    auth utils setup

---
+++ src/lib/authUtils.ts
@@ -0,0 +1,80 @@
+export type UserRole = "ADMIN" | "CUSTOMER";
+
+export const authRoutes = ["/login", "/register", "/forgot-password", "/reset-password", "/verify-email"];
+
+export const isAuthRoute = (pathname: string) => {
+    return authRoutes.some((route: string) => route === pathname);
+};
+
+export type RouteConfig = {
+    exact: string[];
+    pattern: RegExp[];
+};
+
+export const commonProtectedRoutes: RouteConfig = {
+    exact: ["/my-profile", "/change-password"],
+    pattern: [],
+};
+
+export const adminProtectedRoutes: RouteConfig = {
+    pattern: [/^\/admin\/dashboard/],
+    exact: [],
+};
+
+export const customerProtectedRoutes: RouteConfig = {
+    pattern: [/^\/dashboard/],
+    exact: ["/payment/success"],
+};
+
+export const isRouteMatches = (pathname: string, routes: RouteConfig) => {
+    if (routes.exact.includes(pathname)) {
+        return true;
+    }
+    return routes.pattern.some((pattern: RegExp) => pattern.test(pathname));
+};
+
+
+export const getRouteOwner = (
+    pathname: string
+): "ADMIN" | "CUSTOMER" | "COMMON" | null => {
+    if (isRouteMatches(pathname, adminProtectedRoutes)) {
+        return "ADMIN";
+    }
+
+    if (isRouteMatches(pathname, customerProtectedRoutes)) {
+        return "CUSTOMER";
+    }
+
+    if (isRouteMatches(pathname, commonProtectedRoutes)) {
+        return "COMMON";
+    }
+
+    return null;
+};
+
+export const getDefaultDashboardRoute = (role: UserRole) => {
+    if (role === "ADMIN") {
+        return "/admin/dashboard";
+    }
+
+    if (role === "CUSTOMER") {
+        return "/dashboard";
+    }
+
+    return "/";
+};
+
+export const isValidRedirectForRole = (redirectPath: string, role: UserRole) => {
+    
+    const routeOwner = getRouteOwner(redirectPath);
+
+    if (routeOwner === null || routeOwner === "COMMON") {
+        return true;
+    }
+
+    if (routeOwner === role) {
+        return true;
+    }
+
+    return false;
+};
